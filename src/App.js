d70e4e6a22bb50bfbac4c5d9c3ec44b3679e3595
IvaniaPires <ivania.n.pires@gmail.com>
2021-09-08 11:30:19 +0200

    change methodes (delete upLeft, downRight, fillRows, FillColumns and add moveLeft, moveRight, MoveUp, Movedown, hasardPosition et hasardValue

--- src/App.js
+++ src/App.js
@@ -7,91 +7,183 @@ class App extends Component {
     board: Array(12).fill(0),
     step:0,
     score:0,
-    history: Array(12).fill(0),
-    aux1:[],
-    aux2:[],
-    aux3:[],
-    aux4:[],
+    history: Array(12).fill(0),   
     startGame : true,    
   };
 
- fillRows = ()=>{ //création d'une copie de chaque ligne du tableau
-    for(let i = 0; i < 16; i++){
-        if(i < 4 ){
-          this.setState({aux1:this.state.aux1.push(this.state.board[i])})          
-        }else if(i < 8 ){
-          this.setState({aux2:this.state.aux2.push(this.state.board[i])})
-        }else if(i < 12 ){
-          this.setState({aux3:this.state.aux3.push(this.state.board[i])})
-        }else{
-          this.setState({aux4:this.state.aux4.push(this.state.board[i])})
-        }  
-      }		
-  }
-
- fillColumns = ()=>{ //création d'une copie de chaque colonne du tableau
-    for(let i = 0; i<16; i++){
-      if(i % 4 === 0 ){
-        this.setState({aux1:this.state.aux1.push(this.state.board[i])})
-      }else if(i % 4 === 1 ){
-        this.setState({aux2:this.state.aux2.push(this.state.board[i])})
-      }else if(i % 4 === 2 ){
-        this.setState({aux3:this.state.aux3.push(this.state.board[i])})
-      }else{
-        this.setState({aux4:this.state.aux4.push(this.state.board[i])})
-      }  
-    }		
-  }
-  upLeft =  (aux)=>{ //résultat quand on joue vers le haut ou vers la gauche
-       
-    for(let i = 0; i<3 ; i++){ 
-      if(aux[i]!==0){
-        for(let j = i+1; j<4; j++){
-          if(aux[j]!==0){
-            if(aux[i]===aux[j]){              
-              aux[j] = 0;						
+ 
+  moveLeft =  ()=>{ //résultat quand on joue vers le haut ou vers la gauche
+    let tab = this.state.board;
+    for(let i = 0; i < 16; i+=4){
+      for(let x = i; x < i+3; x++){ 
+        if(tab[x] !== 0){
+          for(let j = x+1; j < i+4; j++){
+            if(tab[j] === tab[i]){              
+                tab[i] += tab[j];              
+                tab[j] = 0;	
+                break;                           					
             }
-            break;					
           }
         }
       }
     }
-    for(let i = 0; i<3; i++){
-      if(aux[i]===0){
-        for (let j = i+1; j<4; j++){
-          if(aux[j]!==0){
-            aux[i]=aux[j];
-            aux[j]=0;
-            break;
+    for(let i = 0; i < 16; i+=4){
+      for(let x = i; x <i+3; x++){
+        if(tab[x] === 0){
+          for (let j = x+1; j< i+4; j++){
+            if(tab[j] !== 0){
+              tab[x] = tab[j];
+              tab[j] = 0;
+              break;
+            }
           }
         }
       }
-    }	
-    return aux;
+    }
+    this.setState({board:tab});      
+  }
+
+  moveRight = ()=>{
+    let tab = this.state.board;
+    for(let i = 0; i < 16; i+=4){
+      for(let x = i+3; x > i ; x--){
+        if(tab[x]!==0){
+          for(let j = x-1; j >= i; j--){
+            if(tab[j] === tab[x]){
+              tab[x] += tab[j];
+              tab[j] = 0;
+              break;
+            }
+          }
+        }
+      }
+    }
+    for(let i = 0; i < 16; i+=4){
+      for(let x = i+3; x>i; x--){
+        if(tab[x] === 0){
+          for(let j = x-1 ; j >= i ; j--){
+            if(tab[j] !== 0){
+              tab[x] = tab[j];
+              tab[j] = 0;
+              break;
+            }
+          }
+        }
+      }
+    }
+    this.setState({board:tab})	
+  }
+
+  moveUp = ()=>{
+    let tab = this.state.board;
+    for(let i = 12; i < 16; i++){
+      for(let x = i-12; x < i ; x+=4){
+        if(tab[x]!==0){
+          for(let j = x+4; j <= i; j+=4){
+            if(tab[j] === tab[x]){
+              tab[x] += tab[j];
+              tab[j] = 0;
+              break;
+            }
+          }
+        }
+      }
+    }
+    for(let i = 12; i < 16; i++){
+      for(let x = i-12; x < i ; x+=4){
+        if(tab[x] === 0){
+          for(let j = x+4; j <= i; j+=4){
+            if(tab[j] !== 0){
+              tab[x] = tab[j];
+              tab[j] = 0;
+              break;
+            }
+          }
+        }
+      }
+    }
+    this.setState({board:tab})
+  }
+
+  moveDown = () =>{
+    let tab = this.state.board;
+    for(let i = 0; i < 4; i++){
+      for(let x = i+12; x < 12; x-=4){
+        if(tab[x]!==0){
+          for(let j = x-4; j >= i; j-=4){
+            if(tab[j] === tab[x]){
+              tab[x] += tab[j];
+              tab[j] = 0;
+              break;
+            }
+          }
+        }
+      }
+    }
+    for(let i = 0; i < 4; i++){
+      for(let x = i+12; x < 12; x-=4){
+        if(tab[x]!==0){
+          for(let j = x-4; j >= i; j-=4){
+            if(tab[j] !== 0){
+              tab[x] = tab[j];
+              tab[j] = 0;
+            }
+          }
+        }
+      }
+    }
+    this.setState({board:tab})
   }
 
   
   handleMove = evt =>{
-    this.setState({aux1 : [],aux2 : [],aux3 : [],aux4 : []});
-    if(evt.key === "ArrowUp"){ //création d'une copie des colonnes de notre tableau et éxecute la function upLeft
-      this.fillColumns();
-      this.setState({aux1:this.upLeft(this.state.aux1),aux2:this.upLeft(this.state.aux2),aux3:this.upLeft(this.state.aux3),aux4:this.upLeft(this.state.aux4)});
-    } else if(evt.key === "ArrowDown"){//création d'une copie des colonnes de notre tableau et éxecute la function downRight
-      this.fillColumns();	
-    } else if(evt.key === "ArrowRight"){//création d'une copie des lignes de notre tableau et éxecute la function downRight
-      this.fillRows();
-    } else if(evt.key === "ArrowLeft"){//création d'une copie des lignes de notre tableau et éxecute la function upLeft
-     this.fillRows();
-    } else {
+    let identical = true;      
+    for (let i = 0; i < 16; i++){
+        if (this.state.board[i] !== this.state.history[i]){
+            identical = false;
+            break;
+        }
+    }
+    if(identical){
       return;
-    }	
+    } else {
+      this.setState({history: this.state.board});    
+      if(evt.key === "ArrowUp"){ 
+        this.moveUp();     
+      } else if(evt.key === "ArrowDown"){
+        this.moveDown();
+      } else if(evt.key === "ArrowRight"){
+        this.moveRight();
+      } else if(evt.key === "ArrowLeft"){
+        this.moveLeft();
+      } else {
+        return;
+      }	
+    }  
+    
   }
 
+  hasardPosition = ()=>{
+    let aux =[];
+    for(let i = 0; i < 16; i++){
+      if(this.state.board[i]===0){
+        aux.push(i);
+      }
+    }
+    return aux[Math.floor(Math.random() * aux.length)];
+  }
+
+  hasardValue = ()=>{
+    let value;
+    (Math.floor(Math.random() * (10)) + 1)===1? value = 4: value = 2;
+    return value; 
+  }
 
   render(){
     document.addEventListener('keydown', evt=> {
       this.handleMove(evt)      
     });
+    if()
     return(
       <Fragment>
         <GameBoard board={this.state.board}/>
